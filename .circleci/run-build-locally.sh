baeea8f0fce041058cd5da68d6d1b54c1bf8736b
Ryosuke Tajima <ryosuke.tajima@opensource-robotics.tokyo.jp>
2018-08-08 15:31:54 +0900

    Add deployment (not sure)

--- .circleci/run-build-locally.sh
+++ .circleci/run-build-locally.sh
@@ -2,7 +2,7 @@
 
 curl --user ${CIRCLE_TOKEN}: \
      --request POST \
-     --form revision=7fe381ea9f78ccb0631e878bb99b1b969dd70ad2 \
      --form config=@config.yml \
      --form notify=false \
      https://circleci.com/api/v1.1/project/github/tork-a/adi_driver/tree/circleci2
+#     --form revision=1c16e315b550a4cd3b11b8db69e1ba24c4f1a3bc \
