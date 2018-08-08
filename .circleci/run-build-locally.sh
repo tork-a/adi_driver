1c16e315b550a4cd3b11b8db69e1ba24c4f1a3bc
Ryosuke Tajima <ryosuke.tajima@opensource-robotics.tokyo.jp>
2018-08-08 13:23:49 +0900

    More adoptation

--- .circleci/run-build-locally.sh
+++ .circleci/run-build-locally.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-CIRCLE_TOKEN=28c9d6b21270b362d25f5ec6c11ee1f0fd340a7f
-
 curl --user ${CIRCLE_TOKEN}: \
      --request POST \
-     --form revision=4c8efba906fc2ccae1f29a72896ef716e8c3c76a \
+     --form revision=7fe381ea9f78ccb0631e878bb99b1b969dd70ad2 \
      --form config=@config.yml \
      --form notify=false \
      https://circleci.com/api/v1.1/project/github/tork-a/adi_driver/tree/circleci2
